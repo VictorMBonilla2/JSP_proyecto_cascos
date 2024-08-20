f840c28c471cf2e6938ff910404b32537464cef4
VictorMBonilla2 <lossg2234@gmail.com>
2024-08-20 18:24:31 -0500

    Avance 2 20/08/2024

--- Prueba_JSP/src/main/webapp/resources/js/Profile.js
+++ Prueba_JSP/src/main/webapp/resources/js/Profile.js
@@ -3,6 +3,8 @@ const divPerfil = document.querySelector(".main_container__profile")
 
 const divDetalles = document.querySelector(".detalles_user_container")
 
+const divPerfil2 = document.querySelector(".user_container_side2")
+
 document.addEventListener("click", (ev)=>{
     if(ev.target.matches(".info_user_container__button")){
         if (divPerfil.classList.contains("modificado")){
@@ -10,6 +12,7 @@ document.addEventListener("click", (ev)=>{
         divPerfil.style.gridTemplateColumns = '34% 66%';
         divDetalles.style.opacity='100%';
         divDetalles.style.display='flex'
+        divPerfil2.style.display='none'
         }
         else{
             divPerfil.classList.add("modificado")
@@ -17,7 +20,33 @@ document.addEventListener("click", (ev)=>{
             divDetalles.style.opacity='0%'
             setTimeout(() => {
                 divDetalles.style.display='none'
+                divPerfil2.style.display='flex'
+                const infoBody = document.querySelector(".info_user_container__body");
+                infoBody.innerHTML = `
+                <form id="userInfoForm">
+                    <div>
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" id="nombre" name="nombre" value="jose">
+                    </div>
+                    <div>
+                        <label for="apellido">Apellido:</label>
+                        <input type="text" id="apellido" name="apellido" value="juan">
+                    </div>
+                    <div>
+                        <label for="fechaNacimiento">Fecha de nacimiento:</label>
+                        <input type="date" id="fechaNacimiento" name="fechaNacimiento" value="1992-03-17">
+                    </div>
+                    <div>
+                        <button type="button" class="formulario_login__button" id="submitForm">Enviar</button>
+                        <button type="button" class="formulario_login__button" id="cancelEdit">Cancelar</button>
+                    </div>
+                </form>
+            `;
             }, "2100");
+
+
+
+
         }
     }else {
         console.log("pues no")
